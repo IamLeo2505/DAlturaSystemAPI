0d83e422133f3b951d9c7f174a7d04e20c0fa937
Pobreton57 <yoandrynunezgarcia@gmail.com>
2024-10-04 22:30:25 -0600

    Update DetalleCredito.cs

--- Modelos/DetalleCredito.cs
+++ Modelos/DetalleCredito.cs
@@ -2,5 +2,8 @@
 {
     public class DetalleCredito
     {
+        public int IDDetalleCredito { get; set; }
+        public date FechaPago { get; set; }
+        public double MontoAbono { get; set; }
     }
 }
