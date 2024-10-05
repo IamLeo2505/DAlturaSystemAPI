129b8e4ed0ce355f8db66445035045799e9894e0
Pobreton57 <yoandrynunezgarcia@gmail.com>
2024-10-04 22:22:16 -0600

    Update Usuario.cs

--- Modelos/Usuario.cs
+++ Modelos/Usuario.cs
@@ -3,6 +3,10 @@
     public class Usuario
     {
       public int IDUsuario { get; set; }
-      public string usuario { get; set; }
+      public string Usuario { get; set; }
+      public string Pass { get; set; }
+      public string Acceso { get; set; }
+      public string Estado { get; set; }
+      public int IDEmpleado { get; set; }
     }
 }
