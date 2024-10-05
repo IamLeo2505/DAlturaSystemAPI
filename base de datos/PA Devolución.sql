b9e7dc5dc03fbfbf01e23a4a21472058a8f755f8
Pobreton57 <yoandrynunezgarcia@gmail.com>
2024-10-04 22:49:02 -0600

    Update PA Devolución.sql

--- base de datos/PA Devolución.sql
+++ base de datos/PA Devolución.sql
@@ -3,46 +3,34 @@ as
 begin
      select 
 	 IdDevolucion, FechaDevolucion, Motivo
-	 from 
+	 from Devolución
 end
 
 
 
-create proc pA_guardar_empleado(
-@nombre varchar(50),
-@apellidos varchar(50),
-@dni varchar(16),
-@telefono varchar(8),
-@direccion varchar(150),
-@estado varchar(10)
+create proc pA_guardar_devolucion(
+@IdDevolución int,
+@FechaDevolución date,
+@Motivo varchar(20)
 ) as 
 begin
-     insert into empleado(nombre, apellidos, dni, telefono, direccion, estado)
-	 values (@nombre, @apellidos, @dni, @telefono, @direccion, @estado)
+     insert into Devolución(IdDevolucion, FechaDevolucion, Motivo)
+	 values (@IdDevolucion, @FechaDevolucion, @Motivoo)
 end
 
 
 
-create proc pA_editar_empleado(
-@idempleado int,
-@nombre varchar(50) null,
-@apellidos varchar(50) null,
-@dni varchar(16) null, 
-@telefono varchar(8)  null, 
-@direccion varchar(150)  null, 
-@estado varchar(10)
+create proc pA_editar_devolucion(
+@IdDevolución int not null,
+@FechaDevolución date not null,
+@Motivo varchar(20) not null
 ) as 
 begin
 
-update empleado set
-
-nombre = isnull(@nombre, nombre),
-apellidos = isnull(@apellidos, apellidos),
-dni = isnull(@dni, dni),
-telefono = isnull(@telefono, telefono),
-direccion = isnull(@direccion, direccion),
-estado = isnull(@estado, estado)
-where idempleado = @idempleado
+update Devolución set
+FechaDevolucion = isnull(@FechaDevolucion, FechaDevolucion),
+Motivo = isnull(@Motivo, Motivo)
+where IdDevolucion = @IdDevolucion
 
 end
 
