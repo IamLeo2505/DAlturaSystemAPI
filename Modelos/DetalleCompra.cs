3a248904642a01b6e394031dd36feaed8e1741f5
unknown101101k <118642472+unknown101101k@users.noreply.github.com>
2024-10-05 21:47:59 -0600

    Add New File

--- Modelos/DetalleCompra.cs
+++ Modelos/DetalleCompra.cs
@@ -2,5 +2,12 @@
 {
     public class DetalleCompra
     {
+        public int iddetallecompra { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+        public decimal total { get; set; }
+        public int idcompra { get; set; } 
+        public int idproducto { get; set; }
     }
 }
+
