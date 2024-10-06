6e01339378398e0288c588008a3bd8d0ee3b4c8e
Pobreton57 <yoandrynunezgarcia@gmail.com>
2024-10-06 14:57:47 -0600

    Update PA Usuario.sql

--- base de datos/PA Usuario.sql
+++ base de datos/PA Usuario.sql
@@ -1,1 +1,45 @@
+create proc pA_lista_devolucion
+as
+begin
+     select 
+	 IdDevolucion, FechaDevolucion, Motivo
+	 from Devolución
+end
 
+
+
+create proc pA_guardar_devolucion(
+@FechaDevolución date,
+@Motivo varchar(20)
+) as 
+begin
+     insert into Devolución(FechaDevolucion, Motivo)
+	 values (@IdDevolucion, @FechaDevolucion, @Motivoo)
+end
+
+
+
+create proc pA_editar_devolucion(
+@IdDevolución int not null,
+@FechaDevolución date not null,
+@Motivo varchar(20) not null
+) as 
+begin
+
+update Devolución set
+FechaDevolucion = isnull(@FechaDevolucion, FechaDevolucion),
+Motivo = isnull(@Motivo, Motivo)
+where IdDevolucion = @IdDevolucion
+
+end
+
+
+create proc pA_eliminar_devolucion(
+@IdDevolucion int
+)
+as 
+begin
+delete from Devolución where IdDevolucion = @IdDevolucion
+end
+
+select * from Devolución
