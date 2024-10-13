e15202d452f347a75e84adc3a0ec421e915b4a57
unknown101101k <118642472+unknown101101k@users.noreply.github.com>
2024-10-12 20:29:45 -0600

    Update Program.cs
    Update ojalá que compile

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Sirve archivos estáticos desde wwwroot
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
