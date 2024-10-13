2181b688e816972715ea08374244077c658be250
unknown101101k <118642472+unknown101101k@users.noreply.github.com>
2024-10-12 20:48:39 -0600

    Bugs Fixed

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,16 +12,17 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
+// Nota: Swagger se mueve al final para que no sobrescriba la página predeterminada
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Swagger y Swagger UI solo si es necesario y en desarrollo
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
